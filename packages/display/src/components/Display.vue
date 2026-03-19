04b664e929a5041fcb7fbbbe3ce5209831283abe
rkusan00 <renato.kusanovic@gmail.com>
2026-03-19 07:55:31 +0100

    Add image overlay zoom

--- packages/display/src/components/Display.vue
+++ packages/display/src/components/Display.vue
@@ -1,17 +1,39 @@
 <template>
   <div class="tce-image-root">
-    <VImg
+    <VOverlay
       v-if="element.data.url"
-      :alt="element.data.alt"
-      :src="element.data.url"
-      class="mx-auto"
+      v-model="isZoomed"
+      :class="{ expanded: isZoomed }"
+      content-class="d-flex align-center justify-center h-100 w-100"
+      close-on-content-click
     >
-      <template #placeholder>
-        <div class="d-flex align-center justify-center fill-height">
-          <VProgressCircular color="blue-grey-darken-3" indeterminate />
-        </div>
+      <template #activator="{ props: overlayProps }">
+        <VImg
+          v-bind="overlayProps"
+          :alt="element.data.alt"
+          :src="element.data.url"
+          class="image-preview mx-auto"
+        >
+          <template #placeholder>
+            <div class="d-flex align-center justify-center fill-height">
+              <VProgressCircular color="blue-grey-darken-3" indeterminate />
+            </div>
+          </template>
+        </VImg>
       </template>
-    </VImg>
+      <VBtn
+        class="position-absolute top-0 right-0 ma-4"
+        color="white"
+        icon="mdi-close"
+        variant="tonal"
+        @click="isZoomed = false"
+      />
+      <img
+        :alt="element.data.alt"
+        :src="element.data.url"
+        style="max-width: 100vh; max-height: 100vh"
+      />
+    </VOverlay>
     <VSheet
       v-else
       class="d-flex justify-center align-center my-2 text-title-large"
@@ -25,11 +47,23 @@
 
 <script setup lang="ts">
 import { Element } from '@tailor-cms/ce-image-manifest';
+import { ref } from 'vue';
 
 defineProps<{ element: Element; userState: any }>();
+
+const isZoomed = ref(false);
 </script>
 
 <style scoped>
-.tce-image-root {
+.image-preview {
+  cursor: zoom-in;
+}
+
+.v-overlay {
+  transition: all 0.3s ease;
+
+  &.expanded {
+    backdrop-filter: blur(18px);
+  }
 }
 </style>

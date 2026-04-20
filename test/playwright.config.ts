98a28c511d1365aac10e626dfeeaf64e62dcfdfb
rkusan00 <renato.kusanovic@gmail.com>
2026-04-20 13:17:06 +0200

    Fix env

--- test/playwright.config.ts
+++ test/playwright.config.ts
@@ -4,7 +4,7 @@ import { config as dotenvConfig } from 'dotenv';
 dotenvConfig();
 
 if (!process.env.PREVIEW_RUNTIME_URL)
-  process.env.PREVIEW_RUNTIME_URL = 'http://localhost:8001';
+  process.env.PREVIEW_RUNTIME_URL = 'http://localhost:8080';
 
 /**
  * See https://playwright.dev/docs/test-configuration.
